6e09a49cc47b78de59b93fe93a445c6864dc4ec5
tom chang <tomlin83@gmail.com>
2020-11-25 15:14:46 +0800

    clear rake

--- app/models/review.rb
+++ app/models/review.rb
@@ -2,5 +2,5 @@ class Review < ApplicationRecord
   belongs_to :restaurant
   validates :content, :rating, presence: true
   validates :rating, numericality: true
-  validates :rating , inclusion: { in: (0..5)}
+  validates :rating , inclusion: { in: (0..5), message: 'Number should be between 0 to 5'}
 end
